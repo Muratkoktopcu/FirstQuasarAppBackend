77d8b9c453da89e5f3c1d79c3ed3642f3cdc31f3
Muratkoktopcu <muratkoktopcu22@gmail.com>
2025-08-18 17:11:37 +0300

    feat(env)=cors içerisindeki bilgiler .env e alındı

--- internal/http/middleware/cors.go
+++ internal/http/middleware/cors.go
@@ -2,16 +2,23 @@ package middleware
 
 import (
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/go-chi/cors"
 )
 
 func CORS(next http.Handler) http.Handler {
+	allowedOrigins := strings.Split(os.Getenv("CORS_ALLOWED_ORIGINS"), ",")
+	allowedMethods := strings.Split(os.Getenv("CORS_ALLOWED_METHODS"), ",")
+	allowedHeaders := strings.Split(os.Getenv("CORS_ALLOWED_HEADERS"), ",")
+	allowCredentials := os.Getenv("CORS_ALLOW_CREDENTIALS") == "true"
+
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
-		AllowCredentials: true,
+		AllowedOrigins:   allowedOrigins,
+		AllowedMethods:   allowedMethods,
+		AllowedHeaders:   allowedHeaders,
+		AllowCredentials: allowCredentials,
 	})
 	return c.Handler(next)
 }
